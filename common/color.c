c52a60c0ff2a37917fd2651bcb0af96c0ae5993f
Levente Kurusa <levex@linux.com>
2018-03-25 15:02:30 +0100

    libcommon: add correctness printer
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- common/color.c
+++ common/color.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-void
+int
 print_availability(int avail)
 {
     printf("[%s]\n", avail ? "\e[32;5;196mYES\e[39m" : "\e[38;5;196mNO\e[39m");
+    return avail;
+}
+
+int
+print_correctness(int c)
+{
+    printf("[%s]\n", c ? "\e[32;5;196mCORRECT\e[39m" : "\e[38;5;196mINCORRECT\e[39m");
+    return c;
 }
--- include/common/color.h
+++ include/common/color.h
@@ -1,6 +1,7 @@
 #ifndef __COMMON_COLOR_H
 #define __COMMON_COLOR_H
 
-void print_availability(int);
+int print_availability(int);
+int print_correctness(int);
 
 #endif /* __COMMON_COLOR_H */
