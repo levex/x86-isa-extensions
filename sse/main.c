cb1fc03fffa4958438d69545fcda14e3c71f5fc3
Levente Kurusa <levex@linux.com>
2018-04-08 12:49:44 +0200

    sse: add a second example (maxps)
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- sse/main.c
+++ sse/main.c
@@ -7,10 +7,13 @@
 extern int is_sse_available(void);
 extern int test_sse_addition(volatile float *, volatile float *,
             volatile float *);
+extern int test_sse_max(volatile float *, volatile float *,
+            volatile float *);
 
 volatile float a[4] = {3.14f, 2.76f, 2.71f, 8.66f};
 volatile float b[4] = {3.14f, 2.12f, 2.11f, 1.22f};
 volatile float c[4] = {6.28f, 4.88f, 4.82f, 9.88f};
+volatile float d[4] = {3.14f, 2.76f, 2.71f, 8.66f};
 volatile float result[4] = {0.00f, 0.00f, 0.00f, 0.00f};
 
 void
@@ -27,6 +30,20 @@ do_examples(void)
         print_correctness(0);
         printf("       target[0]: %f [1]: %f [2]: %f [3]: %f\n",
                     c[0], c[1], c[2], c[3]);
+        printf("       actual[0]: %f [1]: %f [2]: %f [3]: %f\n",
+                    result[0], result[1], result[2], result[3]);
+    }
+    printf("     Testing SSE vector maxing... ");
+    test_sse_max(a, b, result);
+    if (result[0] - d[0] < 0.001f &&
+            result[1] - d[1] < 0.001f &&
+            result[2] - d[2] < 0.001f &&
+            result[3] - d[3] < 0.001f)
+        print_correctness(1);
+    else {
+        print_correctness(0);
+        printf("       target[0]: %f [1]: %f [2]: %f [3]: %f\n",
+                    d[0], d[1], d[2], d[3]);
         printf("       actual[0]: %f [1]: %f [2]: %f [3]: %f\n",
                     result[0], result[1], result[2], result[3]);
     }
